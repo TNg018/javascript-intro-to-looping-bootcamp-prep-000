6cf0b39a4b0666b951cd85a160cda1b5ee33afb7
Tien Nguyen <pri859@sudomail.com>
2018-02-21 23:53:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (var i = 1; i < 100; i++) {
-  console.log( "Hello World the " + i + " time" );
+function forLoop(array) {
+  for (var i = 0; i < 26; i++) {
+    console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.")
+  }
 }
