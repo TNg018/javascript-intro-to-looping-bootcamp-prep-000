6383a18fb5f78a9dbf5475c1e8ddeeed62e11b28
Tien Nguyen <pri859@sudomail.com>
2018-02-22 00:31:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
   for (let i = 0; i < 25; i++) {
 if (false) {
-  console.log("I am ${i} strange loop.");
-} else {
   console.log("I am 1 strange loop.");
+} else {
+  console.log("I am ${i} strange loop.");
   }
 }
