4ffdb5a8e2186f09f4d1c2a56d9b2be7f3e11ea7
Tien Nguyen <pri859@sudomail.com>
2018-02-22 00:16:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   console.log("I am 1 strange loop.")
-  if (false) {
+  else {
     return console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.")
   }
 }
